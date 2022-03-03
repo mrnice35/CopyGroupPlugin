0bcdc649f5d834ddc2c5832fd24ec4b0e3d955e7
gavrilenkovi@bk.ru <gavrilenkovi@bk.ru>
2022-03-03 22:21:10 +0300

    1

--- CopyGroupPlugin/Class1.cs
+++ CopyGroupPlugin/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using System;
@@ -15,21 +16,81 @@ namespace CopyGroupPlugin
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            UIDocument uiDoc = commandData.Application.ActiveUIDocument;//Доступ к документу
-            Document doc = uiDoc.Document;
-           Reference reference = uiDoc.Selection.PickObject(ObjectType.Element, "Выберите группу объектов");//Выбор группы
-            Element element = doc.GetElement(reference);
-            Group group = element as Group;//преобразовали к типу група
+            try
+            {
+                UIDocument uiDoc = commandData.Application.ActiveUIDocument;//Доступ к документу
+                Document doc = uiDoc.Document;
 
-            XYZ point = uiDoc.Selection.PickPoint("Выберите точку");// выбор точки
+                GroupPickFilter groupPickFilter = new GroupPickFilter();
+                Reference reference = uiDoc.Selection.PickObject(ObjectType.Element, groupPickFilter, "Выберите группу объектов");//Выбор группы
+                Element element = doc.GetElement(reference);
+                Group group = element as Group;//преобразовали к типу група
+                XYZ groupCenter = GetElementCenter(group);
+                Room room = GetRoomByPoint(doc, groupCenter);
+                XYZ roomCenter = GetElementCenter(room);
+                XYZ offset = groupCenter - roomCenter;
 
-            Transaction transaction = new Transaction(doc);
-            transaction.Start("Копирование группы объектов");
-            doc.Create.PlaceGroup(point, group.GroupType);
-            transaction.Commit();
 
+                XYZ point = uiDoc.Selection.PickPoint("Выберите точку");// выбор точки
+                Room roomNew = GetRoomByPoint(doc, point);
+                XYZ roomNewCenter = GetElementCenter(roomNew);
+                XYZ pointNew = roomNewCenter + offset;
+
+                Transaction transaction = new Transaction(doc);
+                transaction.Start("Копирование группы объектов");
+                var groupNew = doc.Create.PlaceGroup(pointNew, group.GroupType);
+                groupNew.get_Parameter(BuiltInParameter.GROUP_OFFSET_FROM_LEVEL).Set(0);
+                transaction.Commit();
+                
+            }
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                return Result.Cancelled;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
             return Result.Succeeded;
+        }
+        public XYZ GetElementCenter (Element element)
+        {
+           BoundingBoxXYZ bounding = element.get_BoundingBox(null);
+            return (bounding.Max + bounding.Min) / 2;
+        }
+        public Room GetRoomByPoint(Document doc, XYZ point)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfCategory(BuiltInCategory.OST_Rooms);
+            foreach (Element e in collector)
+            {
+                Room room = e as Room;
+                if (room!=null)
+                {
+                    if (room.IsPointInRoom(point))
+                    {
+                        return room;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+    public class GroupPickFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            if (elem.Category.Id.IntegerValue == (int)BuiltInCategory.OST_IOSModelGroups)
+                return true;
+            else
+                return false;
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
         }
     }
 }
- 
+  
